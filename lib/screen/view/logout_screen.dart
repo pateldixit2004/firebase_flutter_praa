159f9c327fc740f7876acecf729385085915c222
dixit <patoliyadixit257@gamil.com>
2023-07-18 14:16:38 +0530

    add product in firebase

--- lib/screen/view/logout_screen.dart
+++ lib/screen/view/logout_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_flutter_pra/screen/controller/user_controller.dart';
+import 'package:firebase_flutter_pra/screen/view/product_add_screen.dart';
 import 'package:firebase_flutter_pra/utils/firebase_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -11,33 +13,80 @@ class Log_Screen extends StatefulWidget {
 }
 
 class _Log_ScreenState extends State<Log_Screen> {
+  UserControler controler = Get.put(UserControler());
+
+  @override
+  void initState() {
+    super.initState();
+    controler.userMap.value = FireBaseHelper.base.userData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      appBar:AppBar(
+    return SafeArea(
+      child: DefaultTabController(
 
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Log Out"),
-          onPressed: () {
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  child: Text("Product"),
+                ),
+                Tab(
+                  child: Text("Add"),
+                )
+              ],
 
-            FireBaseHelper.base.SignOut();
-            Get.offAllNamed('/home');
-
-
-          },
-        ),
-      ),
-      drawer: Drawer(
-        child: Column(
-          children: [
-            CircleAvatar(
-              radius: 30,
             ),
-          ],
+          ),
+          body: TabBarView(
+            children: [
+              AddProuctScreen(),
+              AddProuctScreen(),
+            ],
+          ),
+          drawer: Drawer(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundImage: NetworkImage(controler.userMap['photo'] == null
+                        ? "https://cdn-icons-png.flaticon.com/128/4140/4140048.png"
+                        : "${controler.userMap["photo"]}"),
+                  ),
+                  SizedBox(
+                    height: 10,
+
+                  ),
+                  Text(controler.userMap['name'] == null
+                      ? ""
+                      : "${controler.userMap['name']}"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(controler.userMap['email'] == null
+                      ? ""
+                      : "${controler.userMap['email']}"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    child: Text("Log Out"),
+                    onPressed: () {
+                      FireBaseHelper.base.SignOut();
+                      Get.offAllNamed('/home');
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
-    ),);
+    );
   }
 }
--- lib/utils/firebase_helper.dart
+++ lib/utils/firebase_helper.dart
@@ -1,94 +1,91 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_flutter_pra/screen/model/peoduct_model.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class FireBaseHelper
-{
-  static FireBaseHelper base=FireBaseHelper();
+class FireBaseHelper {
+  static FireBaseHelper base = FireBaseHelper();
 
-  FirebaseAuth auth=FirebaseAuth.instance;
-
-  Future<String> anonymouslyGet()
-  async {
-    try
-        {
-          await auth.signInAnonymously();
-          return "Succes";
-        }
-        catch(e)
-          {
-            return "Fallied";
-          }
-  }
-
-
-  bool chechUser()
-  {
-    auth.currentUser;
-    return auth==null;
-  }
-
-
-  Future<void> SignOut()
-  async {
-    await auth.signOut();
-    // GoogleSignIn().signOut();
-  }
-
-  Future<String> createUser(String email,String password)
-  async {
-    try{
-      await auth.createUserWithEmailAndPassword(email: email, password: password);
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firestore=FirebaseFirestore.instance;
+  Future<String> anonymouslyGet() async {
+    try {
+      await auth.signInAnonymously();
       return "Succes";
-    }catch(e)
-    {
+    } catch (e) {
+      return "Fallied";
+    }
+  }
+
+  bool chechUser() {
+    auth.currentUser;
+    return auth == null;
+  }
+
+  Future<void> SignOut() async {
+    await auth.signOut();
+    GoogleSignIn().signOut();
+  }
+
+  Future<String> createUser(String email, String password) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return "Succes";
+    } catch (e) {
       return "$e";
     }
-
-
   }
 
-
-  Future<String> signin(String email,String password)
-  async {
-    try{
+  Future<String> signin(String email, String password) async {
+    try {
       await auth.signInWithEmailAndPassword(email: email, password: password);
       return "Succes";
-    }catch(e)
-    {
+    } catch (e) {
       return "$e";
     }
-
-
   }
 
-
-  Future<String> googleSignIn()
-  async {
-
+  Future<String> googleSignIn() async {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
     // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
 
     // Create a new credential
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
-    try{
+    try {
       await auth.signInWithCredential(credential);
       return "Succes";
-    }catch(e)
-    {
+    } catch (e) {
       return "$e";
     }
   }
 
-  void userData()
-  {
-
+  Map<String, dynamic> userData() {
+    User? user = auth.currentUser;
+    var email = user!.email;
+    var photo = user.photoURL;
+    var name = user.displayName;
+    return {"email": email, "img": photo, "name": name};
   }
 
+  // =====================================
+
+  void insertproductDeta(ProductModel model) {
+
+    firestore.collection("Product").add({
+      "name": model.name,
+      "price": model.price,
+      "cate": model.cate,
+      "img": model.img,
+      "dec": model.dec
+    });
+  }
 }
